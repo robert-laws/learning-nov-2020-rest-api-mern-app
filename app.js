1f613e93855dabf9aa2f003db4732423396decd3
Robert Laws <roblaws@me.com>
2020-11-01 15:35:43 +0300

    beginning add error handling

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const itemsRoutes = require('./routes/items');
 
 const app = express();
 
-app.use(itemsRoutes);
+app.use('/api/items', itemsRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || 'An unknown error occurred.' });
+});
 
 app.listen(5000);
--- routes/items.js
+++ routes/items.js
@@ -4,9 +4,76 @@ const { builtinModules } = require('module');
 
 const router = express.Router();
 
+itemsData = [
+  {
+    id: '1',
+    group: 'equipment',
+    title: 'Microphone',
+    description: 'USB microphone to enhance audio output',
+  },
+  {
+    id: '2',
+    group: 'equipment',
+    title: 'HDMI Cable',
+    description: 'Connect HDMI enabled devices and monitors',
+  },
+  {
+    id: '3',
+    group: 'equipment',
+    title: 'Kindle E-Book Reader',
+    description:
+      'Read pre-loaded e-books on academic and pleasure reading topics',
+  },
+  {
+    id: '4',
+    group: 'equipment',
+    title: 'Apple AV Connector',
+    description: 'Connect iPad and Mac Computer to AV devices',
+  },
+  {
+    id: '5',
+    group: 'software',
+    title: 'iMovie',
+    description: 'Apple software used to create videos',
+  },
+  {
+    id: '6',
+    group: 'learning',
+    title: 'LinkedIn Learning',
+    description: 'Thousands of video tutorials on software and other skills',
+  },
+];
+
 router.get('/', (req, res, next) => {
-  console.log('GET Request in items');
-  res.json({ message: 'it works...' });
+  const data = itemsData;
+
+  if (!itemsData) {
+    // return res
+    //   .status(404)
+    //   .json({ message: 'Could not find a place with the provided id.' });
+    const error = new Error('Could not find items.');
+    error.code = 404;
+    throw error;
+  }
+
+  res.json(itemsData);
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  let item = itemsData.find((item) => item.id === id);
+
+  if (!item) {
+    // return res
+    //   .status(404)
+    //   .json({ message: 'Could not find a place with the provided id.' });
+    const error = new Error('Could not find an item with the provided id.');
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json(item);
 });
 
 module.exports = router;
